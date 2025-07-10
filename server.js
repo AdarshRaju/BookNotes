4d30e8fc4a716badd08616fa5b41747db7a5012b
AdarshRaju <adarsh_rg5@yahoo.co.in>
2025-07-10 20:55:34 +1000

    authentication features added

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ import axios from 'axios';
 import sanitizeHtml from 'sanitize-html';
 import env from "dotenv";
 
+import userRouter, {db, setuppassport} from "./users.js";
+import bodyParser from "body-parser";
+
+import bcrypt from "bcrypt";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
+import session from "express-session";
+
+
 const app = express();
 const port = 3000;
 env.config();
@@ -13,141 +23,207 @@ app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 
 
-const db = new pg.Client({
-    user: process.env.SQL_USER,
-    host: process.env.SQL_HOST,
-    database: process.env.SQL_DATABASE,
-    password: process.env.SQL_PASSWORD,
-    port: process.env.SQL_PORT,
-    pool_mode: process.env.SQL_MODE
-   
+// All incoming request will now be checked for a session cookie with a session id.
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+setuppassport();
+
+// Uses all the routes imported from ./users.js. The external modules involved will be automatically used internally as required.
+
+app.use("/users", userRouter);
+
+
+// Normal database logic for logged in users
+// The home page should be directed to home.ejs, if not authenticated and to 
+app.get("/", async (req, res) => {
+    if (req.isAuthenticated()) {
+        res.redirect("/yourcollection");
+    } else {
+        res.redirect("/users/home");
+    }
 });
 
-db.connect();
-
-app.get("/", async (req, res) => {
+app.get("/yourcollection", async (req, res) => {
     // populate with existing information from the database
-    
-    var dbquery;
-    const sortval = req.query.sorted || "";
-    switch(sortval){
-        case "title": 
-            dbquery = "ORDER BY title ASC";
-            break;
-        case "rating":
-            dbquery = "ORDER BY rating ASC";
-            break;
-        case "time":
-            dbquery = "ORDER BY date ASC";
-            break;
-        case "author":
-        dbquery = "ORDER BY author ASC";
-        break;
-    }
-    let dbresult;
-    if (dbquery){
-     dbresult = await db.query(`SELECT * FROM bookreviewlist ${dbquery}`);
-    } else {
-     dbresult = await db.query(`SELECT * FROM bookreviewlist`);
-    }
-    // console.log("The data fetched from the db is: ", dbresult.rows);
-    res.render("index.ejs", {dbdata: dbresult.rows, sorted: sortval});
-})
+    if(req.isAuthenticated()) {
+        var loggedinemail = req.user.email;
+        // console.log("The loggedinemail returned from the server is: ", loggedinemail);
+        var dbquery;
+        var dbsortord;
+        const sortval = req.query.sorted || "";
+        const sortorder = req.query.sortord || "";
 
+       console.log("sortorder received is: ", sortorder);
+
+        switch (sortorder) {
+        case "ascending":
+        dbsortord = "ASC";
+        break;
+
+        case "descending":
+        dbsortord = "DESC";
+        break;
+        };
+
+        switch(sortval){
+            case "title": 
+                dbquery = "ORDER BY title "+dbsortord;
+                break;
+            case "rating":
+                dbquery = "ORDER BY rating "+dbsortord;
+                break;
+            case "time":
+                dbquery = "ORDER BY date "+dbsortord;
+                break;
+            case "author":
+            dbquery = "ORDER BY author "+dbsortord;
+            break;
+        };
+
+        let dbresult;
+         console.log("dbquery value is: ", dbquery);
+        if (dbquery){
+        dbresult = await db.query(`SELECT * FROM bookreviewlist WHERE email = $1 ${dbquery}`, [loggedinemail]);
+        } else {
+        dbresult = await db.query(`SELECT * FROM bookreviewlist WHERE email = $1`, [loggedinemail]);
+        }
+        // console.log("The data fetched from the db is: ", dbresult.rows);
+        res.render("index.ejs", {dbdata: dbresult.rows, sorted: sortval});
+    } else {
+        res.redirect("/users/home");
+    };
+});
+
+// This is just for sending the page for the user to input the actual new book information
 app.get("/addnewbookpage", (req, res) =>{
-    res.render("addnewbook.ejs");
+    if(req.isAuthenticated()) {
+        res.render("addnewbook.ejs");
+    } else {
+        res.redirect("/users/home");
+    };
 });
 
 app.get("/editbook/:id", async (req, res) =>{
-    const bookid = sanitizeHtml(req.params.id);
-    console.log("The book id received is: ", bookid);
-    const editdbresult = await db.query(`SELECT * FROM bookreviewlist WHERE id = $1`, [bookid]);
-    console.log("The book title db result for edit is: ", editdbresult.rows[0].title);
-    res.render("editbook.ejs", {bookdata: editdbresult.rows[0]});
-
-})
+    if(req.isAuthenticated()) {
+        const bookid = sanitizeHtml(req.params.id);
+        console.log("The book id received is: ", bookid);
+        const editdbresult = await db.query(`SELECT * FROM bookreviewlist WHERE id = $1`, [bookid]);
+        console.log("The book title db result for edit is: ", editdbresult.rows[0].title);
+        res.render("editbook.ejs", {bookdata: editdbresult.rows[0]});
+    } else {
+        res.redirect("/users/home");
+    };
+});
 
 app.get("/delete/:id", async (req, res) =>{
-    const bookid = sanitizeHtml(req.params.id).toString();
-    console.log("The id received from delete is: ", bookid);
-    await db.query(`DELETE FROM bookreviewlist WHERE id = $1`, [bookid]);
-    res.redirect("/");
-})
+    if(req.isAuthenticated()) {
+        const bookid = sanitizeHtml(req.params.id).toString();
+        console.log("The id received from delete is: ", bookid);
+        await db.query(`DELETE FROM bookreviewlist WHERE id = $1`, [bookid]);
+        res.redirect("/");
+    } else {
+        res.redirect("/users/home");
+    };
+});
 
 app.post("/postnewbook", async (req, res) => {
-    
-    let imgsrc;
-    // collect all the input information in variables
-    let newbooktitle = sanitizeHtml(req.body.booktitle);
-    let newbookauthor = sanitizeHtml(req.body.bookauthor);
-    let newbooksummary = sanitizeHtml(req.body.booksummary);
-    let newbookrating = sanitizeHtml(req.body.rating);
-    
-    // get the book ISBN no
-    const newtitlewords = newbooktitle.split(" ").join("+");
-    const newauthorwords = newbookauthor.split(" ").join("+");
-    try{
-    const response = await axios.get(`https://openlibrary.org/search.json?title=${newtitlewords}`);
-    // Below is the isbn13 location from google api(not used here as it needs api key)
-    // const isbn13google = response.data.items[0].volumeInfo.industryIdentifiers[1].identifier;
-
-    
-    // const isbn13 = response.data.docs[0].ia.find(item => item.startsWith(“ISBN”)).slice(5)
-    var isbn13;
-
-    if(response.data.docs[0].ia.find(item => item.startsWith("isbn"))){
-    isbn13 = response.data.docs[0].ia.find(item => item.startsWith("isbn")).slice(5);
-    console.log("The ISBN13 no obtained is: " + isbn13);
-    } else {isbn13=""}
-    
-
-    } catch(err) {
-        console.error("The error fetching isbn is: " + err);
-    }
-    if (isbn13){
-        console.log("The isbn13 value is: " + isbn13);
+    if(req.isAuthenticated()) {
+        var loggedinemail = req.user.email;
+        let imgsrc;
+        // collect all the input information in variables
+        let newbooktitle = sanitizeHtml(req.body.booktitle);
+        let newbookauthor = sanitizeHtml(req.body.bookauthor);
+        let newbooksummary = sanitizeHtml(req.body.booksummary);
+        let newbookrating = sanitizeHtml(req.body.rating);
+        
+        // get the book ISBN no
+        const newtitlewords = newbooktitle.split(" ").join("+");
+        const newauthorwords = newbookauthor.split(" ").join("+");
         try{
-        const time = new Date();
-        await db.query('INSERT INTO bookreviewlist(title, author, rating, summary, isbn, date) VALUES ($1, $2, $3, $4, $5, $6)', [newbooktitle, newbookauthor, newbookrating, newbooksummary, isbn13, time]);
-        } catch(error) { console.error("error inserting into database with isbn13 present is: " + error)}
-    }else{
-        console.log("ISBN13 does not exist!");
-        try{
-        const time = new Date();
-        await db.query('INSERT INTO bookreviewlist(title, author, rating, summary, isbn, date) VALUES ($1, $2, $3, $4, $5, $6)', [newbooktitle, newbookauthor, newbookrating, newbooksummary, "", time]);
-        } catch(error) { console.error("error inserting into database without isbn13 present is: " + error)}
+        const response = await axios.get(`https://openlibrary.org/search.json?title=${newtitlewords}`);
+
+        var isbn13;
+
+        if(response.data.docs[0].ia.find(item => item.startsWith("isbn"))){
+        isbn13 = response.data.docs[0].ia.find(item => item.startsWith("isbn")).slice(5);
+        console.log("The ISBN13 no obtained is: " + isbn13);
+        } else {isbn13=""}
+        
+
+        } catch(err) {
+            console.error("The error fetching isbn is: " + err);
+        }
+        if (isbn13){
+            console.log("The isbn13 value is: " + isbn13);
+            try{
+            const time = new Date();
+            await db.query('INSERT INTO bookreviewlist(title, author, rating, summary, isbn, date, email) VALUES ($1, $2, $3, $4, $5, $6, $7)', [newbooktitle, newbookauthor, newbookrating, newbooksummary, isbn13, time, loggedinemail]);
+            } catch(error) { console.error("error inserting into database with isbn13 present is: " + error)}
+        }else{
+            console.log("ISBN13 does not exist!");
+            try{
+            const time = new Date();
+            await db.query('INSERT INTO bookreviewlist(title, author, rating, summary, isbn, date, email) VALUES ($1, $2, $3, $4, $5, $6, $7)', [newbooktitle, newbookauthor, newbookrating, newbooksummary, "", time, loggedinemail]);
+            } catch(error) { console.error("error inserting into database without isbn13 present is: " + error)}
+        };
+        res.redirect("/");
+    } else {
+        res.redirect("/users/home");
     };
-    res.redirect("/");
-
-    // get the book cover pic url src
-
-    // if (isbn13){
-    //     imgsrc = `https://covers.openlibrary.org/b/isbn/${isbn13}-L.jpg`;
-    // } else {imgsrc = ""};
-
-    // Add the information to the database
-    // console.log("The book title received is: " + req.body.booktitle);
-    // console.log("The book rating received is: " + req.body.rating);
+    
 })
 
 app.post("/posteditedbook", async (req, res) =>{
 
-    const bookid = sanitizeHtml(req.body.editbookid);
-    let updatedbooktitle = sanitizeHtml(req.body.editbooktitle);
-    let updatedbookauthor = sanitizeHtml(req.body.editbookauthor);
-    let updatedbooksummary = sanitizeHtml(req.body.editbooksummary);
-    let updatedbookrating = sanitizeHtml(req.body.editrating);
+    if(req.isAuthenticated()) {
+        const bookid = sanitizeHtml(req.body.editbookid);
+        let updatedbooktitle = sanitizeHtml(req.body.editbooktitle);
+        let updatedbookauthor = sanitizeHtml(req.body.editbookauthor);
+        let updatedbooksummary = sanitizeHtml(req.body.editbooksummary);
+        let updatedbookrating = sanitizeHtml(req.body.editrating);
 
-    let updatedtime = new Date();
+        let updatedtime = new Date();
 
-    await db.query('UPDATE bookreviewlist SET title=$1, author=$2, rating=$3, summary=$4, date=$5 WHERE id=$6', [updatedbooktitle, updatedbookauthor, updatedbookrating, updatedbooksummary, updatedtime, bookid]);
-    
-    res.redirect("/");
-})
+        await db.query('UPDATE bookreviewlist SET title=$1, author=$2, rating=$3, summary=$4, date=$5 WHERE id=$6', [updatedbooktitle, updatedbookauthor, updatedbookrating, updatedbooksummary, updatedtime, bookid]);
+        
+        res.redirect("/");
+     } else {
+        res.redirect("/users/home");
+    };
+});
 
 app.post("/sort", (req, res) =>{
-    const sortquery = sanitizeHtml(req.body.sortby);
-    res.redirect(`/?sorted=${sortquery}`);
+    if(req.isAuthenticated()) {
+        const sortquery = sanitizeHtml(req.body.sortby);
+        const sortorder = sanitizeHtml(req.body.sortorder);
+        res.redirect(`yourcollection/?sorted=${sortquery}&sortord=${sortorder}`);
+     } else {
+        res.redirect("/users/home");
+    };
+});
+
+app.get("/bookview/:id", async (req, res) =>{
+    if(req.isAuthenticated()) {
+        const bookid = sanitizeHtml(req.params.id);
+        const editdbresult = await db.query(`SELECT * FROM bookreviewlist WHERE id = $1`, [bookid]);
+        res.render("bookview.ejs", {bookdata: editdbresult.rows[0]});
+    } else {
+    res.redirect("/users/home");
+    };
+});
+
+app.get("/about", (req, res) =>{
+    let loggedstatus = req.user ? true : false;
+    res.render("about.ejs", {loggedstatus: loggedstatus});
 })
 
 app.listen(port, console.log(`Server has started on port ${port}`));
---
+++ users.js
@@ -0,0 +1,258 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import bcrypt from "bcrypt";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
+import session from "express-session";
+import env from "dotenv";
+import sanitizeHtml from 'sanitize-html';
+
+
+env.config();
+
+
+// const app = express();
+// const port = 3000;
+const saltRounds = 10;
+
+
+// No need to use a different db object again, as different tables can be used from the same database
+export const db = new pg.Client({
+    user: process.env.SQL_USER,
+    host: process.env.SQL_HOST,
+    database: process.env.SQL_DATABASE,
+    password: process.env.SQL_PASSWORD,
+    port: process.env.SQL_PORT,
+    pool_mode: process.env.SQL_MODE
+   
+});
+
+db.connect();
+
+const router = express.Router();
+export default router;
+
+router.get("/home", (req, res) => {
+  
+  res.render("users/home.ejs");
+});
+
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.get("/register", (req, res) => {
+  // console.log("/users/register link was accessed.");
+  res.render("users/register.ejs");
+});
+
+router.get("/logout", (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/users/home");
+  });
+});
+
+
+// The callback function needs to list the permissions to be asked from the user
+router.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+// This is for google authenticated users specifically
+router.get(
+  "/auth/google/callbackuri",
+  passport.authenticate("google", {
+    successRedirect: "/yourcollection",
+    failureRedirect: "login",
+  })
+);
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    // This needs to be an absolute path
+    successRedirect: "../yourcollection",
+    // This needs to be a relative path
+    failureRedirect: "login",
+  })
+);
+
+// Registration is only applicable for local password
+router.post("/register", async (req, res, next) => {
+  const email = sanitizeHtml(req.body.username);
+  const password = sanitizeHtml(req.body.password);
+
+  try {
+    // No need to register if an email is found, but need to ask users to link gmail oauth if no password found
+    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+    // Case 1: An email (user) - and corresponding linked data in database was found
+    if (checkResult.rows.length > 0) {
+
+      // Case 1a: A password entry was found
+      if (checkResult.rows[0].password){
+          passport.authenticate("local", (err, user, info) => {
+            if(err){return next(err)};
+            if(!user){return res.redirect("login")};
+            req.logIn(user, (err) =>{
+              if(err) {return next(err)};
+              return res.redirect("../yourcollection")
+            });
+          })(req,res, next);
+      } 
+      // Case 1b: Only a google oauth verification was found
+      else if (checkResult.rows[0].gmail_verified == true){
+        
+        // Adding a new password entry to the existing email row entry
+         bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) {
+          console.error("Error hashing password:", err);
+        } else {
+          const result = await db.query("UPDATE users SET password = $1 WHERE email = $2 RETURNING *", [hash, email]
+          );
+          const user = result.rows[0];
+          req.login(user, (err) => {
+            console.log("successfully added a password to the existing gmail oauth entry");
+            res.redirect("/yourcollection");
+          });
+        }
+      });
+
+      }
+      
+    } 
+    // Case 2: An email (user) data entry was not found in the database
+    else {
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) {
+          console.error("Error hashing password:", err);
+        } else {
+          const result = await db.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
+            [email, hash]
+          );
+          const user = result.rows[0];
+          req.login(user, (err) => {
+            console.log("success");
+            res.redirect("/yourcollection");
+          });
+        }
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//TODO: Create the post route for submit.
+//Handle the submitted data and add it to the database
+
+export function setuppassport(){
+  passport.use(
+    "local",
+    new Strategy(async function verify(username, password, cb) {
+      try {
+      //  We need to SELECT from SQL just to check if an email entry exist in the database
+        const result = await db.query("SELECT * FROM users WHERE (email = $1)", [
+          username,
+        ]);
+        // Case 1: An email entry exists
+        if (result.rows.length > 0) {
+          //  Case 1a: A local hashed password entry exists for the email entry
+          if(result.rows[0].password && result.rows[0].password !== "google"){
+            const user = result.rows[0];
+            const storedHashedPassword = user.password;
+            bcrypt.compare(password, storedHashedPassword, (err, valid) => {
+              if (err) {
+                console.error("Error comparing passwords:", err);
+                return cb(err);
+              } else {
+                if (valid) {
+                  return cb(null, user);
+                } else {
+                  return cb(null, false);
+                }
+              }
+            });
+          } 
+          // Case 1b: Only a google oauth entry exists for the email entry
+          else {
+            return cb("Please click on Sign In With Google");
+          }
+
+        } 
+        // Case 2: An email entry was not found
+        else {
+          return cb("User not found");
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    })
+  );
+
+  passport.use(
+    "google",
+    new GoogleStrategy(
+      // Need to include the client id and secret in .env. Also need to register and set up our app with google.
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/users/auth/google/callbackuri",
+        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+      },
+      async (accessToken, refreshToken, profile, cb) => {
+        try {
+          // console.log(profile);
+          const result = await db.query("SELECT * FROM users WHERE (email = $1)", [
+            profile.email,
+          ]);
+          // Case 1: No email (user) entry was found in the database
+          if (result.rows.length === 0) {
+            // If not user entry is found, it is created immediately (and set "g" value)
+            const newUser = await db.query(
+              "INSERT INTO users (email, gmail_verified, password) VALUES ($1, $2, $3) RETURNING *",
+              [profile.email, true, "google"]
+            );
+            return cb(null, newUser.rows[0]);
+          } 
+          // Case 2: An email (user) entry was found in the database
+          else {
+          // Case 2a: Google verified entry was true in the email entry
+            if(result.rows[0].gmail_verified == true){
+              return cb(null, result.rows[0]);
+            } 
+          // Case 2b: Google verified entry was false in the email entry and is set to true
+            else if(result.rows[0].gmail_verified == false){
+              const updatedUser = await db.query(
+              "UPDATE users SET gmail_verified = $1 WHERE email= $2 RETURNING *",
+              [true, profile.email,]
+            );
+            return cb(null, updatedUser.rows[0]);
+            }
+          }
+        } catch (err) {
+          return cb(err);
+        }
+      }
+    )
+  );
+
+  // The whole user information will be attached to req.user from below, instead of user.id
+  passport.serializeUser((user, cb) => {
+    cb(null, user);
+  });
+
+  passport.deserializeUser((user, cb) => {
+    cb(null, user);
+  });
+};
